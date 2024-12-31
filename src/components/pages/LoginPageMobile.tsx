2790a54ed0a9c2a52f7ad2649d66eca99874a507
IanSkelskey <IanSkelskey@users.noreply.github.com>
2024-12-31 22:03:41 +0000

    Prettified Code!

--- src/components/pages/LoginPageMobile.tsx
+++ src/components/pages/LoginPageMobile.tsx
@@ -18,7 +18,10 @@ import ForgotPasswordModal from '../modals/ForgotPasswordModal';
 import logo from '../../assets/logo.svg';
 
 interface LoginPageMobileProps {
-    login: (email: string, password: string) => Promise<{ user?: any; error?: string }>;
+    login: (
+        email: string,
+        password: string
+    ) => Promise<{ user?: any; error?: string }>;
 }
 
 const LoginPageMobile: React.FC<LoginPageMobileProps> = ({ login }) => {
@@ -26,11 +29,14 @@ const LoginPageMobile: React.FC<LoginPageMobileProps> = ({ login }) => {
         localStorage.getItem('rememberedEmail') !== null
     );
     const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState(localStorage.getItem('rememberedEmail') || '');
+    const [email, setEmail] = useState(
+        localStorage.getItem('rememberedEmail') || ''
+    );
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [isForgotPasswordModalOpen, setForgotPasswordModalOpen] = useState(false);
+    const [isForgotPasswordModalOpen, setForgotPasswordModalOpen] =
+        useState(false);
 
     const handleClickShowPassword = () => setShowPassword(!showPassword);
 
@@ -60,7 +66,9 @@ const LoginPageMobile: React.FC<LoginPageMobileProps> = ({ login }) => {
                 control={
                     <Checkbox
                         checked={rememberMe}
-                        onChange={(event) => setRememberMe(event.target.checked)}
+                        onChange={(event) =>
+                            setRememberMe(event.target.checked)
+                        }
                         color="primary"
                         sx={{
                             padding: 0.5,
@@ -82,87 +90,119 @@ const LoginPageMobile: React.FC<LoginPageMobileProps> = ({ login }) => {
 
     return (
         <>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-					alignItems: 'center',
-					height: '100vh',
-					margin: 2,
-				}}
-			>
-				<img
-					src={logo}
-					alt="App Logo"
-					width={32}
-					height={32}
-					style={{ margin: 8, borderRadius: '50%' }}
-				/>
-				<Typography variant="h5" gutterBottom fontWeight="bold" align="center">
-					Sign In to the Spa Dashboard
-				</Typography>
-				<Typography variant="body2" gutterBottom color="textSecondary" align="center">
-					Welcome, please sign in to continue
-				</Typography>
-				<form onSubmit={handleSubmit} style={{ width: '100%' }}>
-					{error && (
-						<Typography color="error" gutterBottom>
-							{error}
-						</Typography>
-					)}
-					<TextField
-						label="Email"
-						required
-						fullWidth
-						margin="normal"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<TextField
-						label="Password"
-						required
-						fullWidth
-						margin="normal"
-						type={showPassword ? 'text' : 'password'}
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<Tooltip
-										title={showPassword ? 'Hide password' : 'Show password'}
-									>
-										<IconButton
-											aria-label="toggle password visibility"
-											onClick={handleClickShowPassword}
-											edge="end"
-										>
-											{showPassword ? <VisibilityOff /> : <Visibility />}
-										</IconButton>
-									</Tooltip>
-								</InputAdornment>
-							),
-						}}
-					/>
-					<RememberMe />
-					<Box
-						sx={{
-							display: 'flex',
-							justifyContent: 'space-between',
-							alignItems: 'center',
-							mt: 2,
-						}}
-					>
-						<Link onClick={() => setForgotPasswordModalOpen(true)} to={''}>
-							<Typography variant="body2">Forgot Password?</Typography>
-						</Link>
-						<Button type="submit" variant="contained" color="primary" disabled={loading}>
-							{loading ? <CircularProgress size={24} /> : 'Sign In'}
-						</Button>
-					</Box>
-				</form>
-			</Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    margin: 2,
+                }}
+            >
+                <img
+                    src={logo}
+                    alt="App Logo"
+                    width={32}
+                    height={32}
+                    style={{ margin: 8, borderRadius: '50%' }}
+                />
+                <Typography
+                    variant="h5"
+                    gutterBottom
+                    fontWeight="bold"
+                    align="center"
+                >
+                    Sign In to the Spa Dashboard
+                </Typography>
+                <Typography
+                    variant="body2"
+                    gutterBottom
+                    color="textSecondary"
+                    align="center"
+                >
+                    Welcome, please sign in to continue
+                </Typography>
+                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                    {error && (
+                        <Typography color="error" gutterBottom>
+                            {error}
+                        </Typography>
+                    )}
+                    <TextField
+                        label="Email"
+                        required
+                        fullWidth
+                        margin="normal"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        label="Password"
+                        required
+                        fullWidth
+                        margin="normal"
+                        type={showPassword ? 'text' : 'password'}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <Tooltip
+                                        title={
+                                            showPassword
+                                                ? 'Hide password'
+                                                : 'Show password'
+                                        }
+                                    >
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </Tooltip>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <RememberMe />
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            mt: 2,
+                        }}
+                    >
+                        <Link
+                            onClick={() => setForgotPasswordModalOpen(true)}
+                            to={''}
+                        >
+                            <Typography variant="body2">
+                                Forgot Password?
+                            </Typography>
+                        </Link>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            disabled={loading}
+                        >
+                            {loading ? (
+                                <CircularProgress size={24} />
+                            ) : (
+                                'Sign In'
+                            )}
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
             <ForgotPasswordModal
                 open={isForgotPasswordModalOpen}
                 onClose={() => setForgotPasswordModalOpen(false)}
