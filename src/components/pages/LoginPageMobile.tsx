a80627a146c3c07d0cf309786e04957ff98312fb
Ian Skelskey <ianskelskey@gmail.com>
2024-12-31 16:57:44 -0500

    Add logo to LoginPageMobile component
    - Imported logo asset and utilized it within the LoginPageMobile component for enhanced branding.
    - The logo is displayed centered above the sign-in header with a circular border for aesthetic purposes.
    
    Release-Note: Display logo on mobile login page for better branding
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- src/components/pages/LoginPageMobile.tsx
+++ src/components/pages/LoginPageMobile.tsx
@@ -15,6 +15,7 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { Link } from 'react-router-dom';
 import ForgotPasswordModal from '../modals/ForgotPasswordModal';
+import logo from '../../assets/logo.svg';
 
 interface LoginPageMobileProps {
     login: (email: string, password: string) => Promise<{ user?: any; error?: string }>;
@@ -81,80 +82,87 @@ const LoginPageMobile: React.FC<LoginPageMobileProps> = ({ login }) => {
 
     return (
         <>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                    padding: 2,
-                }}
-            >
-                <Typography variant="h5" gutterBottom fontWeight="bold" align="center">
-                    Sign In to the Spa Dashboard
-                </Typography>
-                <Typography variant="body2" gutterBottom color="textSecondary" align="center">
-                    Welcome, please sign in to continue
-                </Typography>
-                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-                    {error && (
-                        <Typography color="error" gutterBottom>
-                            {error}
-                        </Typography>
-                    )}
-                    <TextField
-                        label="Email"
-                        required
-                        fullWidth
-                        margin="normal"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        label="Password"
-                        required
-                        fullWidth
-                        margin="normal"
-                        type={showPassword ? 'text' : 'password'}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <Tooltip
-                                        title={showPassword ? 'Hide password' : 'Show password'}
-                                    >
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            edge="end"
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                                        </IconButton>
-                                    </Tooltip>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                    <RememberMe />
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            mt: 2,
-                        }}
-                    >
-                        <Link onClick={() => setForgotPasswordModalOpen(true)} to={''}>
-                            <Typography variant="body2">Forgot Password?</Typography>
-                        </Link>
-                        <Button type="submit" variant="contained" color="primary" disabled={loading}>
-                            {loading ? <CircularProgress size={24} /> : 'Sign In'}
-                        </Button>
-                    </Box>
-                </form>
-            </Box>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+					margin: 2,
+				}}
+			>
+				<img
+					src={logo}
+					alt="App Logo"
+					width={32}
+					height={32}
+					style={{ margin: 8, borderRadius: '50%' }}
+				/>
+				<Typography variant="h5" gutterBottom fontWeight="bold" align="center">
+					Sign In to the Spa Dashboard
+				</Typography>
+				<Typography variant="body2" gutterBottom color="textSecondary" align="center">
+					Welcome, please sign in to continue
+				</Typography>
+				<form onSubmit={handleSubmit} style={{ width: '100%' }}>
+					{error && (
+						<Typography color="error" gutterBottom>
+							{error}
+						</Typography>
+					)}
+					<TextField
+						label="Email"
+						required
+						fullWidth
+						margin="normal"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<TextField
+						label="Password"
+						required
+						fullWidth
+						margin="normal"
+						type={showPassword ? 'text' : 'password'}
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<Tooltip
+										title={showPassword ? 'Hide password' : 'Show password'}
+									>
+										<IconButton
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											edge="end"
+										>
+											{showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
+									</Tooltip>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<RememberMe />
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							mt: 2,
+						}}
+					>
+						<Link onClick={() => setForgotPasswordModalOpen(true)} to={''}>
+							<Typography variant="body2">Forgot Password?</Typography>
+						</Link>
+						<Button type="submit" variant="contained" color="primary" disabled={loading}>
+							{loading ? <CircularProgress size={24} /> : 'Sign In'}
+						</Button>
+					</Box>
+				</form>
+			</Box>
             <ForgotPasswordModal
                 open={isForgotPasswordModalOpen}
                 onClose={() => setForgotPasswordModalOpen(false)}
